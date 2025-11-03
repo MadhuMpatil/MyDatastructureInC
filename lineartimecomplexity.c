459cb7bc4391c281fa50e7c8c9265d52e2760098
MadhuMpatil <patilm83098@gmail.com>
2025-11-03 12:52:15 +0530

    lineartimecomplexity.c

---
+++ lineartimecomplexity.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int n;
+    clock_t start, end;
+    double time_taken;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("\nTime Complexity: O(1)\n");
+
+    start = clock();  // Start timer
+
+    // Executes only once
+    int square = n * n;
+
+    end = clock();  // End timer
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Square of %d = %d\n", n, square);
+    printf("Execution Time = %f seconds\n", time_taken);
+
+    return 0;
+}
