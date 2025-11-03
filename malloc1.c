b33a86417d50f955580af4e5cace96b94c016c62
MadhuMpatil <patilm83098@gmail.com>
2025-11-03 11:49:52 +0530

    Create malloc1.c

---
+++ malloc1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+        int *ptr;
+        ptr=(int*)malloc(sizeof(int));
+        if(ptr==NULL)
+        {
+                printf("memory allocation failed\n");
+                return 1;
+        }
+        *ptr=25;
+        printf("value stored at allocated memory:%d\n",*ptr);
+        free(ptr);
+        return 0;
+}
