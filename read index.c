53c8fcbac896527a47eee2430d9a0f655e78943f
MadhuMpatil <patilm83098@gmail.com>
2025-11-04 15:04:07 +0530

    read index.c

---
+++ read index.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a[]={1,2,3,4,5};
+    int n=sizeof(a)/sizeof(a[0]);
+    int index=0;
+    if(index>=0 && index<n)
+    {
+        printf("%d",a[index]);
+    }
+}
