a194c75032059e660d80f264444f7f3ef3e3bc6e
MadhuMpatil <patilm83098@gmail.com>
2025-11-04 21:13:59 +0530

    Delete at beginning.c
    Refactor code to remove redundancy and improve clarity.

--- Delete at beginning.c
+++ Delete at beginning.c
@@ -2,18 +2,20 @@
 #define CAP 50
 int main()
 {
-int a[CAP]={10,20,30,40};
-int n=4;
-if(n>0){
-for(int i=0;i<n;i++){
-a[i]=a[i+1];
-}
-n--;    
-}
-else
-{
-printf("array is empty");
-}
-for(int i=0;i<n;i++)
-printf("%d ",a[i]);
+  printf("Madhu Patil\n");
+  printf("1SV24cs065\n");
+  int a[CAP]={10,20,30,40};
+  int n=4;
+  if(n>0){
+    for(int i=0;i<n;i++){
+      a[i]=a[i+1];
+    }
+    n--;    
+  }
+  else
+  {
+    printf("array is empty");
+  }
+  for(int i=0;i<n;i++)
+    printf("%d ",a[i]);
 }
